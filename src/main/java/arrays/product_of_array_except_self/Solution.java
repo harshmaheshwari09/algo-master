3ecb66dccc344897c5d078eb67c6bd83681cceb1
harshmaheshwari09 <bittz09@gmail.com>
2026-08-10 15:38:04 +0530

    adding sol for "productExceptSelf"

---
+++ src/main/java/arrays/product_of_array_except_self/Solution.java
@@ -0,0 +1,23 @@
+package arrays.product_of_array_except_self;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] ans = new int[nums.length];
+        Arrays.fill(ans, 1);
+        // prefix calculation
+        int product = 1;
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] *= product;
+            product *= nums[i];
+        }
+        // suffix traversal
+        product = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] *= product;
+            product *= nums[i];
+        }
+        return ans;
+    }
+}
